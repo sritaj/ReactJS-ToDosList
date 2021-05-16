6e61597b95f99e3589224ba2a52edd1499be1c90
Sritaj <sritajp@gmail.com>
2021-05-16 12:06:03 +0530

    Added ToDos.js for displaying all the added ToDos

---
+++ src/Components/ToDos.js
@@ -0,0 +1,9 @@
+import React, { useContext } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { FaCheckDouble } from "react-icons/fa";
+import { ToDoContext } from "../Context/ToDoContext";
+import { REMOVE_TODO } from "../Context/action.types";
+
+const ToDos = () => {};
+
+export default ToDos;
