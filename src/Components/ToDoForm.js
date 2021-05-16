11e4315360389f3426542cf4528aa012d590af3f
Sritaj <sritajp@gmail.com>
2021-05-16 12:00:57 +0530

    Added logics for adding ToDo using dispatch

--- src/Components/ToDoForm.js
+++ src/Components/ToDoForm.js
@@ -13,15 +13,38 @@ import { ToDoContext } from "../Context/ToDoContext";
 import { ADD_TODO } from "../Context/action.types";
 
 const ToDoForm = () => {
+  const [toDoString, setToDoString] = useState("");
+  const { dispatch } = useContext(ToDoContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (toDoString === "") {
+      return alert("Please enter a To Do");
+    }
+
+    const todo = {
+      toDoString,
+      id: v4(),
+    };
+    dispatch({
+      type: ADD_TODO,
+      payload: todo,
+    });
+
+    setToDoString("");
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <InputGroup>
           <Input
             type="text"
             name="todo"
             id="todo"
-            placeholder="Your next Todo" //TODO Value, OnChange
+            placeholder="Your next Todo"
+            value={toDoString}
+            onChange={(e) => setToDoString(e.target.value)}
           />
           <InputGroupAddon addonType="prepend">
             <Button
